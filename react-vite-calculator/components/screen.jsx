d956151cf5b89331a2d9ac9994bcfbafb35947df
John <jrnoden@gmail.com>
2024-09-16 11:39:14 +0100

    Adds logic for screen

--- react-vite-calculator/components/screen.jsx
+++ react-vite-calculator/components/screen.jsx
@@ -1,5 +1,14 @@
-const Screen = ()=>{
-
+const Screen = ({ input, result })=>{
+    return (
+        <section className="card">
+            <div className="input">
+                <h2>{input}</h2>
+            </div>
+            <div className="result">
+                <h2>{result}</h2>
+            </div>
+        </section>
+    )
 }
 
 export default Screen
